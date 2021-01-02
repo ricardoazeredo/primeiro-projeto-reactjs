9f49a053cd2ab99f5323927a566589f7b1ece3b8
Ricardo Azeredo <ricoazeredo@yahoo.com.br>
2021-01-02 20:14:12 -0300

    navigating between routes

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+
 import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
@@ -77,7 +79,7 @@ const Dashboard: React.FC = () => {
       
       <Repositories>
         {repositories.map(repository => (
-          <a key={repository.full_name} href="">
+          <Link key={repository.full_name} to={`/repositories/${repository.full_name}`}>
             <img 
               src={repository.owner.avatar_url} 
               alt={repository.owner.login}
@@ -88,7 +90,7 @@ const Dashboard: React.FC = () => {
             </div>
 
             <FiChevronRight size={20} />
-          </a>  
+          </Link>  
         ))}      
         
       </Repositories>
